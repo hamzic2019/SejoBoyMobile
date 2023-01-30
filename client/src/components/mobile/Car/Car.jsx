db973bfe896f3d0bb36113746f0ce0aadc59065e
hamzic2019 <hamzicharis96@gmail.com>
2023-01-30 21:11:55 +0100

    new212

--- client/src/components/mobile/Car/Car.jsx
+++ client/src/components/mobile/Car/Car.jsx
@@ -1,11 +1,24 @@
 import React, {memo} from 'react'
 import './Car.css';
 
+import {AiOutlineRight, AiFillStar} from 'react-icons/ai'
+
 const Car = () => {
   return (
     <div className='carWrapper'>
       <div className="carwrapperImage"></div>
-      <h1>AUTOMOBIL</h1>
+      <div className='carwrapperImageUnderDetails'>
+        <h1>Audi A6</h1>
+        <p>$120 / Day</p>
+        <div className='carwrapperImageUnderDetailsScorWrapper'>
+            <AiFillStar className='carwrapperImageUnderDetailsIcon' /> 
+            <p>4.95</p>
+        </div>
+        <div className='carwrapperImageSeeDetails'>
+            See Details
+            <AiOutlineRight className='carwrapperImageSeeDetailsIcon' />
+        </div>
+      </div>
     </div>
   )
 }
