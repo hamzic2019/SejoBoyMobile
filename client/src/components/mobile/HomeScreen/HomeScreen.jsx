d2d67d8176b71a8bd067a7ccdb9119c71a94873f
hamzic2019 <hamzicharis96@gmail.com>
2023-01-30 17:30:40 +0100

    d

--- client/src/components/mobile/HomeScreen/HomeScreen.jsx
+++ client/src/components/mobile/HomeScreen/HomeScreen.jsx
@@ -4,6 +4,10 @@ import './HomeScreen.css';
 const HomeScreen = () => {
   return (
     <div className='homeScreenWrapper'>
+        <div>
+          <h1>Let'Start a New Experience</h1>
+          <p>Choose your favourite car, rent it and enjoy new experience with Sejo Boy Mobile!</p>
+        </div>
         <button>Let's Start</button>
     </div>
   )
